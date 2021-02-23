bec67436f962e8406e1e96187ff639153014ba22
Sabino Fernandez <sabinofernandez0@gmail.com>
2021-02-22 19:19:24 -0500

    adding css

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,16 +1,22 @@
-const Character = () => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const Character = async () => {
+  const id = getHash();
+  const character = await getData(id);
   const view = `
-    <div class="Characters-inner">
-      <article class="Characters-card">
-        <img src="image" alt="name">
-        <h2>Nombre</h2>
+    <div class="characters-inner">
+      <article class="characters-card">
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
       </article>
-      <article class="Characters-card">
-        <h3>Episodios: </h3>
-        <h3>Estado: </h3>
-        <h3>Especie: </h3>
-        <h3>Género: </h3>
-        <h3>Origen: </h3>
+      <article class="characters-card">
+        <h3>Episodios: <span>${character.episode.length}</span></h3>
+        <h3>Estado: <span>${character.status}</span></h3>
+        <h3>Especie: <span>${character.species}</span></h3></h3>
+        <h3>Género: <span>${character.gender}</span></h3></h3>
+        <h3>Origen: <span>${character.origin.name}</span></h3></h3>
+        <h3>Última locación: <span>${character.location.name}</span></h3></h3>
       </article>
     </div>
   `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,9 @@ import getData from '../utils/getData';
 const Home = async () => {
   const characters = await getData();
   const view = `
-  <div class="Characters">
+  <div class="characters">
   ${characters.results.map(character => `
-    <article class="Characters-item">
+    <article class="characters-item">
       <a href="#/${character.id}/">
         <img src="${character.image}" alt="${character.name}">
         <h2>${character.name}</h2>
